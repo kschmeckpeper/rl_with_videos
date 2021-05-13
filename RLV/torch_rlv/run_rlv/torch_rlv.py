fc58a65cf9167f534ce48a872db9fccb757cbe02
simonr98 <simonreisch@gmail.com>
2021-05-13 23:54:13 +0100

    create experiment class and input as dictionary

--- RLV/torch_rlv/run_rlv/torch_rlv.py
+++ RLV/torch_rlv/run_rlv/torch_rlv.py
@@ -1,8 +1,19 @@
-from RLV.torch_rlv.executor.run_experiment import run_experiment
+from RLV.torch_rlv.executor.experiment import Experiment
 
 
 def main():
-    run_experiment("acrobot", "sac", 2500, 1000, 256, 256, 0.0003)
+    acrobot_test_config = {
+        'action_space_type': 'discrete',
+        'env_name': 'acrobot',
+        'algo_name': 'algo_name',
+        'n_actions': 2500,
+        'pre_steps': 1000,
+        'layer1_size': 256,
+        'layer2_size': 256,
+        'lr': 0.003
+    }
+    acrobot_test_experiment = Experiment(acrobot_test_config)
+    acrobot_test_experiment.run_experiment()
 
 
 if __name__ == "__main__":
