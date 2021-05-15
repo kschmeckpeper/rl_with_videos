0c38cd2df9d7154d0f9bbb2a4224b9cf4a1af764
simonr98 <simonreisch@gmail.com>
2021-05-15 21:40:41 +0100

    integrate CW2

--- RLV/torch_rlv/run_rlv/torch_rlv.py
+++ RLV/torch_rlv/run_rlv/torch_rlv.py
@@ -1,20 +1,18 @@
 from RLV.torch_rlv.executor.experiment import Experiment
 
 
-def main():
-    acrobot_test_config = {
-        'action_space_type': 'discrete',
-        'env_name': 'acrobot',
-        'algo_name': 'algo_name',
-        'n_actions': 250,
-        'pre_steps': 1000,
-        'layer1_size': 256,
-        'layer2_size': 256,
-        'lr': 0.003
-    }
-    acrobot_test_experiment = Experiment(acrobot_test_config)
+def run_torch_rlv(config):
+    acrobot_test_experiment = Experiment(config)
     acrobot_test_experiment.run_experiment()
 
 
-if __name__ == "__main__":
-    main()
+run_torch_rlv({
+    'action_space_type': 'discrete',
+    'env_name': 'acrobot',
+    'algo_name': 'algo_name',
+    'n_actions': 250,
+    'pre_steps': 1000,
+    'layer1_size': 256,
+    'layer2_size': 256,
+    'lr': 0.003
+})
