fixed minor errors and typos, bug in value_target calculation still present

RLV/torch_rlv/algorithms/sac/sac_agent.py
@@ -1,7 +1,7 @@
 import torch as T
 import torch.nn.functional as F
 from RLV.torch_rlv.buffer.replay_buffer import ReplayBuffer
-from RLV.torch_rlv.models.sac_networks import ActorNetwork, CriticNetwork, ValueNetwork
+from RLV.torch_rlv.models.sac_networks import ActorNetwork,ActorNetworkDiscrete, CriticNetwork, ValueNetwork
 
 
 def get_agent(env, action_space_type, experiment):
@@ -152,8 +152,8 @@ class AgentDiscrete:
         self.batch_size = batch_size
         self.n_actions = n_actions
 
-        self.actor = ActorNetwork(alpha, input_dims, n_actions=n_actions, fc1_dims=layer1_size, fc2_dims=layer2_size,
-                                  name='actor', max_action=env.action_space.high)
+        self.actor = ActorNetworkDiscrete(alpha, input_dims, n_actions=n_actions, fc1_dims=layer1_size, fc2_dims=layer2_size,
+                                  name='actor', max_action=env.action_space.n)
         self.critic_1 = CriticNetwork(beta, input_dims, n_actions=n_actions, fc1_dims=layer1_size, fc2_dims=layer2_size,
                                       name='critic_1')
         self.critic_2 = CriticNetwork(beta, input_dims, n_actions=n_actions, fc1_dims=layer1_size, fc2_dims=layer2_size,
@@ -167,7 +167,7 @@ class AgentDiscrete:
 
     def choose_action(self, observation):
         state = T.Tensor([observation]).to(self.actor.device)
-        actions, _ = self.actor.sample(state)
+        actions, _, _ = self.actor.sample(state)
 
         return actions.cpu().detach().numpy()[0]
 

RLV/torch_rlv/models/sac_networks.py
@@ -1,4 +1,6 @@
 import os
+
+import torch
 import torch as T
 import torch.nn.functional as F
 import torch.nn as nn
@@ -19,7 +21,7 @@ class CriticNetwork(nn.Module):
         self.checkpoint_dir = chkpt_dir
         self.checkpoint_file = os.path.join(self.checkpoint_dir, name + '_sac')
 
-        self.fc1 = nn.Linear(self.input_dims[0] + n_actions, self.fc1_dims)
+        self.fc1 = nn.Linear(self.input_dims[0] + 1, self.fc1_dims)
         self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
         self.q = nn.Linear(self.fc2_dims, 1)
 
@@ -29,7 +31,9 @@ class CriticNetwork(nn.Module):
         self.to(self.device)
 
     def forward(self, state, action):
-        action_value = self.fc1(T.cat([state, action], dim=1))
+        action = torch.reshape(action, (-1, 1))
+        temp = T.cat((state, action), dim=1)
+        action_value = self.fc1(temp)
         action_value = F.relu(action_value)
         action_value = self.fc2(action_value)
         action_value = F.relu(action_value)
@@ -186,7 +190,7 @@ class ActorNetworkDiscrete(nn.Module):
         z = z.float() * 1e-8
         log_action_probabilities = T.log(action_probs + z)
 
-        return action, log_action_probabilities, action_probs
+        return action, log_action_probabilities.view(-1), action_probs.view(-1)
 
     def save_checkpoint(self):
         T.save(self.state_dict(), self.checkpoint_file)