ce805ff0001a42d10e9cefd2d1514b0f061305ee
simonr98 <simonreisch@gmail.com>
2021-05-24 17:02:49 +0100

    add observational data dictionary as Sac input argument

--- RLV/torch_rlv/algorithms/rlv/rlv.py
+++ RLV/torch_rlv/algorithms/rlv/rlv.py
@@ -21,19 +21,14 @@ class RLV:
         self.inverse_model = InverseModelNetwork(beta=0.0003, input_dims=13)
         self.iterations = 500  # TODO
 
-    def get_data_acrobot(self, action_free=False):
+    def fill_action_free_buffer_acrobot(self):
         observation = self.env.reset()
         for _ in range(0, self.agent.batch_size):
             act = self.agent.choose_action(observation)
             action = np.zeros(3)
             action[act] = 1
             observation_, reward, done, info = self.env.step(act)
-
-            if action_free:
-                self.agent.remember_action_free(observation, action, reward, observation_, done)
-            else:
-                self.agent.remember(observation, action, reward, observation_, done)
-
+            self.agent.remember_action_free(observation, action, reward, observation_, done)
             if done:
                 observation = self.env.reset()
             else:
@@ -41,36 +36,45 @@ class RLV:
 
     def run(self):
         p_steps = 100
-        for x in range(0, self.iterations):
-            self.get_data_acrobot(action_free=True)
-            self.get_data_acrobot(action_free=False)
+        for _ in range(0, self.iterations):
+            self.fill_action_free_buffer_acrobot()
 
+        for x in range(0, self.iterations):
             state_obs, target, reward, next_state_obs, done_obs \
                 = self.agent.memory_action_free.sample_buffer(self.agent.batch_size)
-
             done_obs = np.reshape(done_obs, (256, 1))
 
+            # get actions and rewards for observational data
             input_inverse_model = T.cat((T.from_numpy(state_obs), T.from_numpy(next_state_obs),
                                          T.from_numpy(done_obs)), dim=1).float()
 
             action_obs_t = self.inverse_model(input_inverse_model)
+
             reward_obs = np.zeros((self.agent.batch_size, 1))
             for __ in range(0, self.agent.batch_size):
                 reward_obs[__] = set_reward(reward[__])
 
+            # define observational data
             action_obs = action_obs_t.detach().numpy()
 
-            for ___ in range(0, self.agent.batch_size):
-                self.agent.remember(state_obs[___], action_obs[___],
-                                    reward_obs[___], next_state_obs[___], done_obs[___])
+            observational_batch = {
+                'state': state_obs,
+                'action': action_obs,
+                'reward': reward_obs,
+                'next_state': next_state_obs,
+                'done_obs': done_obs
+            }
 
-            s = SAC(env_name=self.env_name, env=self.env, agent=self.agent,
-                    n_games=1, pre_steps=p_steps, score_history=self.score_history)
+            # perform sac based on initial data obtained by environment step plus additional
+            # observational data
+            s = SAC(env_name=self.env_name, env=self.env, memory_mixed=True, agent=self.agent,
+                    n_games=1, pre_steps=p_steps, score_history=self.score_history,
+                    additional_data=observational_batch)
             s.run(cnt=x)
             self.score_history = s.get_score_history()
             p_steps = 0
 
-            #Update Inverse Model
+            # Update Inverse Model
             target_t = T.from_numpy(target).float()
             self.inverse_model.optimizer.zero_grad()
             loss = self.inverse_model.criterion(action_obs_t, target_t)
